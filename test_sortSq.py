5d07e3208650b74ef52c9dcf8564d23f71cd4a37
zheng-jikun <74566082+zheng-jikun@users.noreply.github.com>
2023-01-11 17:45:52 +0800

    Update test_sortSq.py

--- test_sortSq.py
+++ test_sortSq.py
@@ -2,17 +2,11 @@ from sortSq import custom_sort
 import pytest
 
 def test_custom_sort():
-    """
-    This test verifies if `custom_sort` function works correctly
-    only for one specific input list.
-    """
-    # Preparation of test data
+
+    # test data
     unsorted_list = [3, 1, 2]
     expected_sorted_list = [1, 2, 3]
 
-    # Run the tested code
     function_return_value = custom_sort(unsorted_list)
 
-    # Verify if the value returned from the function matches
-    # the expected value.
     assert function_return_value == expected_sorted_list
